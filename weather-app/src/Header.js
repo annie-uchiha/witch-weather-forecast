181cd09190f878ffbdba69ede569d40ee2610566
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-05-31 16:50:50 +0300

    black cat animation

--- weather-app/src/Header.js
+++ weather-app/src/Header.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Header.scss";
 
 const Header = () => {
+  const [startAnimation, setStartAnimation] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setStartAnimation(true);
+    }, 10000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <header className="header">
       <img
-        src="./assets/header-banner.png"
+        src="../assets/header-banner.png"
         alt="Header"
         className="header-image"
       />
+      {startAnimation && (
+        <img
+          src="../assets/black-cat-run.gif"
+          alt="Black Cat"
+          className="cat-gif"
+        />
+      )}
     </header>
   );
 };
